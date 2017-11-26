21367d7208aac99b6ad987ca2fb21609c872701e
Tom West <tomrichardwest@gmail.com>
2017-11-26 22:11:34 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,116 @@
+module.exports = {
+	'rules': {
+		'indent': [
+			2,
+			'tab',
+			{
+				'SwitchCase': 1
+			}
+		],
+		'quotes': [
+			2,
+			'single'
+		],
+		'linebreak-style': [
+			2,
+			'unix'
+		],
+		'semi': [
+			2,
+			'always'
+		],
+		'eol-last': 2,
+		'newline-after-var': [
+			2,
+			'always'
+		],
+		'vars-on-top': 2,
+		'no-unused-vars': 2,
+		'no-console': 1,
+		'no-alert': 2,
+		'no-multi-spaces': 2,
+		'no-multiple-empty-lines': [
+			2,
+			{
+				'max': 1
+			}
+		],
+		'space-infix-ops': 2,
+		'array-bracket-spacing': [
+			'error',
+			'always'
+		],
+		'comma-spacing': [
+			'error',
+			{
+				'before': false,
+				'after': true
+			}
+		],
+		'comma-dangle': 'error',
+		'react/jsx-uses-react': 1,
+		'react/jsx-uses-vars': 1,
+		'react/jsx-curly-spacing': [
+			2, {
+				'when': 'always'
+			}
+		],
+		'react/prop-types': 2,
+		'jsx-quotes': [
+			'error',
+			'prefer-double'
+		],
+		'template-curly-spacing': [ 'error', 'always' ],
+		'prefer-const': 2,
+		'space-before-function-paren': [
+			'error', {
+				'anonymous': 'always',
+				'named':
+				'never'
+			}
+		],
+		'arrow-parens': [
+			'error',
+			'as-needed'
+		],
+		'object-curly-spacing': [
+			'error',
+			'always'
+		],
+
+		'import/no-unresolved': [
+			2, {}
+		],
+		'import/named': 2,
+		'import/namespace': 2,
+		'import/default': 2,
+		'import/export': 2
+	},
+	'parserOptions': {
+		'ecmaVersion': 6,
+		'sourceType': 'module',
+		'ecmaFeatures': {
+			'jsx': true,
+			'experimentalObjectRestSpread': true
+		}
+	},
+	'extends': [
+		'eslint:recommended'
+	],
+	'plugins': [
+		'react',
+		'import'
+	],
+	'env': {
+		'browser': true,
+		'node': true,
+		'es6': true,
+		'jest': true
+	},
+	'settings': {
+		'react': {
+			'pragma': 'React',
+			'version': '15.0'
+		}
+	}
+}
